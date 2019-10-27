21275de40cab0c689a4b4e46960af7225daaf2f5
KarloParagas <karloparagas@gmail.com>
2019-10-26 18:45:21 -0700

    Add project files.

---
+++ SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace SimpleCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.operand1TextBox = new System.Windows.Forms.TextBox();
+            this.operatorTextBox = new System.Windows.Forms.TextBox();
+            this.operand2TextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.Label();
+            this.operand1Error = new System.Windows.Forms.Label();
+            this.operatorError = new System.Windows.Forms.Label();
+            this.operand2Error = new System.Windows.Forms.Label();
+            this.resultError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Operand 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Operator:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Operand 2:";
+            // 
+            // operand1TextBox
+            // 
+            this.operand1TextBox.Location = new System.Drawing.Point(117, 34);
+            this.operand1TextBox.Name = "operand1TextBox";
+            this.operand1TextBox.Size = new System.Drawing.Size(100, 20);
+            this.operand1TextBox.TabIndex = 3;
+            this.operand1TextBox.TextChanged += new System.EventHandler(this.inputChange_TextChanged);
+            // 
+            // operatorTextBox
+            // 
+            this.operatorTextBox.Location = new System.Drawing.Point(117, 74);
+            this.operatorTextBox.Name = "operatorTextBox";
+            this.operatorTextBox.Size = new System.Drawing.Size(100, 20);
+            this.operatorTextBox.TabIndex = 4;
+            this.operatorTextBox.TextChanged += new System.EventHandler(this.inputChange_TextChanged);
+            // 
+            // operand2TextBox
+            // 
+            this.operand2TextBox.Location = new System.Drawing.Point(117, 115);
+            this.operand2TextBox.Name = "operand2TextBox";
+            this.operand2TextBox.Size = new System.Drawing.Size(100, 20);
+            this.operand2TextBox.TabIndex = 5;
+            this.operand2TextBox.TextChanged += new System.EventHandler(this.inputChange_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Result:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(142, 206);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 9;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // resultBox
+            // 
+            this.resultBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.resultBox.Location = new System.Drawing.Point(117, 163);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(100, 23);
+            this.resultBox.TabIndex = 10;
+            // 
+            // operand1Error
+            // 
+            this.operand1Error.AutoSize = true;
+            this.operand1Error.Location = new System.Drawing.Point(223, 34);
+            this.operand1Error.Name = "operand1Error";
+            this.operand1Error.Size = new System.Drawing.Size(0, 13);
+            this.operand1Error.TabIndex = 11;
+            // 
+            // operatorError
+            // 
+            this.operatorError.AutoSize = true;
+            this.operatorError.Location = new System.Drawing.Point(223, 74);
+            this.operatorError.Name = "operatorError";
+            this.operatorError.Size = new System.Drawing.Size(0, 13);
+            this.operatorError.TabIndex = 12;
+            // 
+            // operand2Error
+            // 
+            this.operand2Error.AutoSize = true;
+            this.operand2Error.Location = new System.Drawing.Point(223, 115);
+            this.operand2Error.Name = "operand2Error";
+            this.operand2Error.Size = new System.Drawing.Size(0, 13);
+            this.operand2Error.TabIndex = 13;
+            // 
+            // resultError
+            // 
+            this.resultError.AutoSize = true;
+            this.resultError.Location = new System.Drawing.Point(226, 163);
+            this.resultError.Name = "resultError";
+            this.resultError.Size = new System.Drawing.Size(0, 13);
+            this.resultError.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 279);
+            this.Controls.Add(this.resultError);
+            this.Controls.Add(this.operand2Error);
+            this.Controls.Add(this.operatorError);
+            this.Controls.Add(this.operand1Error);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.operand2TextBox);
+            this.Controls.Add(this.operatorTextBox);
+            this.Controls.Add(this.operand1TextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox operand1TextBox;
+        private System.Windows.Forms.TextBox operatorTextBox;
+        private System.Windows.Forms.TextBox operand2TextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label resultBox;
+        private System.Windows.Forms.Label operand1Error;
+        private System.Windows.Forms.Label operatorError;
+        private System.Windows.Forms.Label operand2Error;
+        private System.Windows.Forms.Label resultError;
+    }
+}
+
---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Clear error messages (if any)
+            ClearErrorMessages();
+
+            string operand1 = operand1TextBox.Text;
+            string op = operatorTextBox.Text;
+            string operand2 = operand2TextBox.Text;
+
+            if (IsValidData(operand1, op, operand2) == true) 
+            {
+                double operand1Num = Convert.ToDouble(operand1);
+                double operand2Num = Convert.ToDouble(operand2);
+
+                //If user divides by zero
+                if (op == "/" && operand1Num == 0 || op == "/" && operand2Num == 0)
+                {
+                    resultError.Text = "Cannot divide by 0";
+                    resultError.ForeColor = Color.Red;
+                }
+                else
+                {
+                    Calculate(operand1Num, operand2Num, op);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks for all types of user input, and displays the correct error message
+        /// </summary>
+        /// <param name="operand1"></param>
+        /// <param name="op"></param>
+        /// <param name="operand2"></param>
+        /// <returns></returns>
+        private bool IsValidData(string operand1, string op, string operand2)
+        {
+            return
+                //Checks for text in textbox
+                IsPresent(operand1, "Operand 1", operand1Error) &&
+                IsPresent(op, "Operator", operatorError) &&
+                IsPresent(operand2, "Operand 2", operand2Error) &&
+
+                //Checks if number input is valid
+                IsDecimal(operand1, operand1Error) && IsDecimal(operand2, operand2Error) &&
+
+                //Checks if number input is within range
+                IsWithinRange(operand1, operand1Error) &&
+                IsWithinRange(operand2, operand2Error) &&
+
+                //Checks for valid operator
+                IsOperator(op, operatorError);
+        }
+
+        /// <summary>
+        /// Clears any error messages displayed in the form, if there are any
+        /// </summary>
+        private void ClearErrorMessages()
+        {
+            operand1Error.Text = string.Empty;
+            operand2Error.Text = string.Empty;
+            operatorError.Text = string.Empty;
+            resultError.Text = string.Empty;
+        }
+
+        /// <summary>
+        /// Checks if user provided any input and displays an error if user provides no input
+        /// </summary>
+        /// <param name="userInput">Textbox input</param>
+        /// <param name="name">Name of the textbox</param>
+        /// <returns></returns>
+        private bool IsPresent(string userInput, string name, Label errMsg) 
+        {
+            if (userInput == "" || userInput == null) 
+            {
+                errMsg.Text = $"{name} is required!";
+                errMsg.ForeColor = Color.Red;
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if user provides a valid integer or decimal, and displays error if it isn't
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <returns></returns>
+        private bool IsDecimal(string userInput, Label errMsg) 
+        {
+            double numInput;
+
+            if (!double.TryParse(userInput, out numInput)) //If input is not a decimal number, display error
+            {
+                errMsg.Text = "Must be a valid integer or decimal";
+                errMsg.ForeColor = Color.Red;
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if user provides a number that is within range
+        /// </summary>
+        /// <returns></returns>
+        private bool IsWithinRange(string userInput, Label errMsg) 
+        {
+            //Convert user's string input to a number
+            double num = Convert.ToDouble(userInput);
+
+            if (num < 0 || num > 1000000) 
+            {
+                errMsg.Text = "Must be between 0 and 1,000,000";
+                errMsg.ForeColor = Color.Red;
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if user provides a valid +, -, *, / operator, and displays error
+        /// if it's not any of those
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <returns></returns>
+        private bool IsOperator(string userInput, Label errMsg) 
+        {
+            if (userInput == "+" || userInput == "-" || userInput == "*" || userInput == "/") 
+            {
+                return true;
+            }
+            errMsg.Text = "Must be a +,  -,  *,  or  /";
+            errMsg.ForeColor = Color.Red;
+            return false;
+        }
+
+        /// <summary>
+        /// Once all input is valid, the calculation is finally perfomed
+        /// </summary>
+        private void Calculate(double operand1, double operand2, string operatorInput) 
+        {
+            //Initialize result
+            double result = 0;
+
+            if (operatorInput == "+") 
+            {
+                result = operand1 + operand2;
+            }
+            else if (operatorInput == "-")
+            {
+                result = operand1 - operand2;
+            }
+            else if (operatorInput == "*")
+            {
+                result = operand1 * operand2;
+            }
+            else if (operatorInput == "/")
+            {
+                result = operand1 / operand2;
+            }
+
+            //Round result to 4 decimal places
+            result = Math.Round(result, 4);
+
+            //Print out the calculation result in the textbox
+            resultBox.Text = result.ToString();
+        }
+
+        /// <summary>
+        /// Exits the form when user clicks the exit button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Clears out the result's display box, if user changes the value 
+        /// of the text boxes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void inputChange_TextChanged(object sender, EventArgs e)
+        {
+            resultBox.Text = "";
+        }
+    }
+}
